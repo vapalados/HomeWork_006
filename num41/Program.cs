3d027c866aca56f211861f39dfb18a597319ee4f
Vasiliy <vapalados@gmail.com>
2022-10-05 20:18:29 +0700

    first commit

---
+++ num41/Program.cs
@@ -0,0 +1,26 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.WriteLine("Введите количеество чисел"); 
+int howmuch = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите числа в количестве " + howmuch);
+int[] Vvod_massiva(int h)
+{
+    int [] massiv = new int[h]; 
+    for (int i = 0; i<h; i++)
+    {
+        massiv[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return massiv;
+}
+void morethannull(int [] mass)
+{
+    int Count = 0;
+    for (int i = 0; i<mass.Length; i++)
+    {
+        if (mass[i]>0)
+        {
+            Count++;
+        }
+    }    
+    Console.WriteLine("Всего больше нуля введено " + Count + " чисел.");
+}
+morethannull(Vvod_massiva(howmuch));
---
+++ num43/Program.cs
@@ -0,0 +1,22 @@
+﻿//Программа, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+double[] Vvod_Peremennih()
+{
+    double[] mass_per = new double[4];
+    Console.WriteLine("Введите значение к1 в формулу y = k1 * x + b1"); 
+    mass_per[0] = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Введите значение b1 в формулу y = k1 * x + b1"); 
+    mass_per[1] = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Введите значение к2 в формулу y = k2 * x + b2"); 
+    mass_per[2] = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Введите значение b2 в формулу y = k2 * x + b2"); 
+    mass_per[3] = Convert.ToDouble(Console.ReadLine());
+    return mass_per;
+}
+void peresechenie(double [] mass)
+{
+    double resx = 0;
+    resx = (mass[3] - mass[1])/(mass[0]-mass[2]);
+    double result = mass[0] * resx + mass[1];
+    Console.WriteLine("Точка пересечения равна: " + resx + "," + result ); 
+}
+peresechenie(Vvod_Peremennih());
